8e50b392251454d46fb403db950ca578666e8451
Santiago-Cuello <santiagonahuelcuello@gmail.com>
2025-06-28 21:59:02 -0300

    Add files via upload

--- webhook.py
+++ webhook.py
@@ -63,7 +63,9 @@ def webhook():
                         enviar_mensaje(numero, respuesta)
 
         return jsonify({"status": "ok"}), 200
-
+@app.route("/", methods=["GET"])
+def home():
+    return "Servidor WhatsApp Bot activo 🚀", 200
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
